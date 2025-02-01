ff8bc5495caac154fbdd1c598c3d56ea134654ca
MUKTAR 'ABDALLAH AL BATTASHI <67161008+MukhtarAlbattashi@users.noreply.github.com>
2025-02-01 17:19:17 +0400

    Update quotations-print.blade.php

--- resources/views/livewire/quotations/quotations-print.blade.php
+++ resources/views/livewire/quotations/quotations-print.blade.php
@@ -105,16 +105,7 @@
                 @endif
             </div>
             <div class="col-md-12 d-flex justify-content-around mt-5">
-                <div class="text-center">
-                    <p>{{__('public.signature')}}</p>
-                    <span>-------------------------------</span>
-                </div>
-                <div class="text-center">
-                    <p>{{__('public.stamp')}}</p>
-                    @if($settings->stamp)
-                        <img src="{{asset($settings->stamp)}}" width="100%" height="100px" alt="">
-                    @endif
-                </div>
+                 <x-invoice-footer/>
             </div>
             <div class="col-md-12">
                 <div class="float-right m-1">
